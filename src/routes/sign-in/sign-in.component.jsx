0cc8e0e3cf2bccfcbf09b6b46c72e5953b3621a9
Abdullah <aabdullahmian@gmail.com>
2024-02-05 00:47:17 +0500

    adding signUp form

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,17 +1,41 @@
-import { signInWithGooglePopup,creeatUserDocumentFromAuth  } from "../../utils/firebase/firebase.utils";
+// import { useEffect } from "react";
+// import { getRedirectResult} from "firebase/auth";
 
+import {
+  // auth,
+  // signInWithGoogleRedirect,
+  signInWithGooglePopup,
+  creeatUserDocumentFromAuth,
+} from "../../utils/firebase/firebase.utils";
 
-const SignIn = () =>{
+import SignUpFrom from "../../components/sign-up-form/sign-up-form.component";
+const SignIn = () => {
+
+  // useEffect(async () => {
+  //   const response = await getRedirectResult(auth);
+  //   if(response){
+  //     const userDocRef = await creeatUserDocumentFromAuth(response.user);  
+  //   }
+  // },[]);
+  
+  // nothing gets logged cuz webPage unmounts and forgets everything
+  // const logGoogleRedirectUser = async () => {
+  //   const { user } = await signInWithGoogleRedirect();
+  //   console.log({ user });
+
   const logGoogleUseer = async () => {
-    const {user} = await signInWithGooglePopup();
+    const { user } = await signInWithGooglePopup();
     const userDocRef = await creeatUserDocumentFromAuth(user);
-  }
+  };
 
+  // }
 
-  return(
+  return (
     <div>
       <button onClick={logGoogleUseer}>Sign In with Google</button>
+      {/* <button onClick={signInWithGoogleRedirect}>Sign In with Google Redirect</button> */}
+      <SignUpFrom />
     </div>
-  )
-}
+  );
+};
 export default SignIn;

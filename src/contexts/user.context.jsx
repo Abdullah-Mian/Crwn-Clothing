829ae12153b14f293470a28f284c54917dd3b3fb
Abdullah <aabdullahmian@gmail.com>
2024-02-08 23:22:06 +0500

    added cart context and shopping svg

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,5 +1,8 @@
 import { createContext, useState, useEffect } from "react";
-import { onAuthStateChangedListener,creeatUserDocumentFromAuth } from "../utils/firebase/firebase.utils";
+import {
+  onAuthStateChangedListener,
+  creeatUserDocumentFromAuth,
+} from "../utils/firebase/firebase.utils";
 
 export const UserContext = createContext({
   currentUser: null,
@@ -12,10 +15,9 @@ export const UserProvider = ({ children }) => {
 
   useEffect(() => {
     const unSubscribe = onAuthStateChangedListener((user) => {
-      if(user){
+      if (user) {
         creeatUserDocumentFromAuth(user);
       }
-
       console.log(user);
       setCurrentUser(user);
       return unSubscribe;
@@ -24,3 +26,4 @@ export const UserProvider = ({ children }) => {
 
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
+

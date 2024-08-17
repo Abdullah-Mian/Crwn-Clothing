48d066e37b15afed9e4be3e77bbc0e7982e36377
abdulah <aabdullahmian@gmail.com>
2024-08-18 00:38:13 +0500

    just a few things left

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,25 +1,13 @@
-import { useContext, Fragment } from "react";
-import { useNavigate } from "react-router-dom";
-import { CategoriesContext } from "../../contexts/categories.context";
-import ProductCard from "../../components/product-card/product-card.component";
+import { Routes, Route } from "react-router-dom";
+import CategoriesPreview from "../../components/categories-preview/categories-preview.component";
+import Category from "../category/category.component";
 import "./shop.styles.scss";
 const Shop = () => {
-  const { categoriesMap } = useContext(CategoriesContext);
-  const navigate = useNavigate();
   return (
-    <Fragment>
-      {Object.keys(categoriesMap).map((title) => (
-        <Fragment key={title}>
-          <h2 onClick={() => navigate(`/${title}`)}>{title}</h2>
-          <div className="products-container">
-            {categoriesMap[title].map(
-              (product, index) =>
-                index < 4 && <ProductCard key={product.id} product={product} />
-            )}
-          </div>
-        </Fragment>
-      ))}
-    </Fragment>
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+      <Route path=':category' element={<Category />}/>
+    </Routes>
   );
 };
 
